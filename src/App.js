e0bbcf01e1404ad93182c4599425ce1d532866f8
marko <marko@marko.com>
2022-12-15 11:20:19 +0100

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import { Route, Redirect, Switch } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import AuthContext from "./store/auth-context";
+
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
@@ -18,10 +24,27 @@ function App() {
   return (
     <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
+   
       <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
+      <Switch>
+        <main>
+          {!authCtx.isLoggedIn && (
+            <Route path="/auth">
+              <AuthPage />
+            </Route>
+          )}
+
+          <Route path="/" exact>
+            <Redirect to="/meals" />
+          </Route>
+
+          <Route path="/meals" exact>
+            <Meals />
+          </Route>
+        </main>
+      </Switch>
+
+   
     </CartProvider>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,56 @@
-
-import { Fragment } from 'react';
-import mealsImage from '../../assets/meals.jpeg';
-import classes from './Header.module.css';
-import HeaderCartButton from './HeaderCartButton';
+import { Fragment, useContext } from "react";
+import { NavLink } from "react-router-dom";
+import mealsImage from "../../assets/meals.jpeg";
+import AuthContext from "../../store/auth-context";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    // history.replace('/');
+  };
+
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Uber Eats</h1>
+        <div className={classes.logo}>
+          <h1>Uber Eats</h1>
+        </div>
+       
+        <nav className={classes.nav}>
+          <ul>
+
+          {!isLoggedIn && (
+            <li>
+              <NavLink to = "/meals"> Meals </NavLink>
+            </li>
+          )}
+          
+          {!isLoggedIn && (
+            <li>
+              <NavLink to="/auth">Login</NavLink>
+            </li>
+          )}
+
+          {isLoggedIn && (
+            <li>
+              <button className={classes.button} onClick={logoutHandler}>
+                Logout
+              </button>
+            </li>
+          )}
+          </ul>
+        </nav>
         <HeaderCartButton onClick={props.onShowCart} />
+        
       </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt='A table full of delicious food!' />
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
